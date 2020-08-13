5e630d8e135ab52a38c701e2fba44346b3d8753a
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-13 17:34:18 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -126,4 +126,134 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(name)
+  game_hash.each do |place, info|
+    info[:players].each do |hash|
+      if hash[:player_name] == name
+        return hash[:points]
+      end
+    end
+  end
+  "Player doesn't exist"
+end
+
+def shoe_size(name)
+  game_hash.each do |place, info|
+    info[:players].each do |hash|
+      if hash[:player_name] == name
+        return hash[:shoe]
+      end
+    end
+  end
+  "Player doesn't exist"
+end
+
+def team_colors(team)
+  game_hash.each do |place, info|
+    if info[:team_name] == team
+      return info[:colors]
+    end
+  end
+end
+
+def team_names
+  names = []
+  game_hash.each do |key, value|
+    names.push(value[:team_name])
+  end
+  names
+end
+
+def player_numbers(team)
+  numbers = []
+  game_hash.each do |key, value|
+    if value[:team_name] == team
+      value[:players].each do |player|
+        numbers.push(player[:number])
+      end
+    end
+  end
+  numbers
+end
+
+def player_stats(name)
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      if player[:player_name] == name
+        return player
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  biggest = []
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      if biggest == []
+        biggest[0] = player[:shoe]
+        biggest[1] = player[:rebounds]
+      end
+      if player[:shoe] > biggest[0]
+        biggest[0] = player[:shoe]
+        biggest[1] = player[:rebounds]
+      end
+    end
+  end
+  biggest[1]
+end
+      
+def most_points_scored 
+  points = [0]
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      if player[:points] > points[0]
+        points[0] = player[:points]
+        points[1] = player[:player_name]
+      end
+    end
+  end
+  points[1]
+end
+
+def winning_team
+  points = {}
+  game_hash.each do |key, value|
+    if !points[value[:team_name]]
+      points[value[:team_name]] = 0
+    end
+    value[:players].each do |player|
+      points[value[:team_name]] += player[:points]
+    end
+  end
+  most = [0]
+  points.each do |team, scores|
+    if points[team] > most[0]
+      most[0] = points[team]
+      most[1] = team
+    end
+  end
+  most[1]
+end
+
+def long_name_steals_a_ton?
+  longest = [0]
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      if player[:player_name].length > longest[0]
+        longest[0] = player[:player_name].length
+        longest[1] = player[:player_name]
+      end
+    end
+  end
+  steals = [0]
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      if player[:steals] > steals[0]
+        steals[0] = player[:steals]
+        steals[1] = player[:player_name]
+      end
+    end
+  end
+  longest[1] == steals[1]
+end
